974a67ce5d6c1a22fb4d6a2bb61b3ad4d6f28940
naomi <naomi@DESKTOP-40O90D3>
2020-03-29 19:31:07 +0200

    prueba de git

---
+++ EJERCICIOSjava/src/ejerciciosjava/EJERCICIOSjava.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciosjava;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author naomi
+ */
+public class EJERCICIOSjava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int num,num2,sor,num1;
+        num1 = (int)(Math.random()*11+1);
+        System.out.println("Introduce numero 1:");
+        num = teclado.nextInt();
+        System.out.println("Introduce numero 2:");
+        num2 = teclado.nextInt();
+        System.out.println("La multiplicacion + numero sorpresa es:"+ num*num2*num1);
+        System.out.println("¿Cual es el numero sorpresa, si este es resultado de multiplicacion");
+        sor = teclado.nextInt();
+        if(num1==sor)
+            System.out.println("Has encontrado el numero sorpresa");
+        else System.out.println("No es el numero sorpresa");
+    }
+    
+}
